14d241682ef6e0ff74c93f66f91e4ab26e367a96
shirshendulayek <shirso.it7@gmail.com>
2026-02-11 19:59:45 +0530

    test for class

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2867
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+from flask import Flask
+app = Flask(__name__)
 
-\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 from flask import Flask\
-app = Flask(__name__)\
-\
-@app.route('/')\
-def hello_world():\
-    return '<h1>Hello from Jenkins CI/CD Pipeline! v1</h1>'\
-\
-if __name__ == '__main__':\
-    app.run(debug=True, host='0.0.0.0')\
+@app.route('/')
+def hello_world():
+    return '<h1>Hello from Jenkins CI/CD Pipeline! v1</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 }
